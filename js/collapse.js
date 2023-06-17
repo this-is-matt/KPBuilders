c1d789907b0eda357ad80dcdbed3b16b367927f8
this-is-matt <mcp6000@gmail.com>
2023-06-17 00:04:57 -0500

    cleanup and finishing touches

--- js/collapse.js
+++ js/collapse.js
@@ -4,7 +4,8 @@ collapseBtns.forEach((collapseBtn) => {
     let content = collapseBtn.nextElementSibling;
 
     collapseBtn.addEventListener('click', (e) => {
-        content.classList.toggle('active');
+        content.classList.toggle('active-content');
         console.log("test");
+        collapseBtn.classList.toggle('active-btn');
     })
 })
